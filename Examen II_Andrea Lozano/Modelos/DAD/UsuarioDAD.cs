f78f53e26722c7c7e8d7dc085aaf5fbdc006e52d
AndreaLozanoCortes <alozanoc@unah.hn>
2021-11-11 21:59:24 -0600

    Inicio

---
+++ Examen II_Andrea Lozano/Modelos/DAD/UsuarioDAD.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_II_Andrea_Lozano.Modelos.DAD
+{
+    class UsuarioDAD
+    {
+    }
+}
