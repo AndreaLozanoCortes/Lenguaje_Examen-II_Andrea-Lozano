a240e05098e74f799dea8c3173dd90296c16572c
AndreaLozanoCortes <alozanoc@unah.hn>
2021-11-11 22:01:28 -0600

    Inicio

--- Examen II_Andrea Lozano/Modelos/DAD/Conexion.cs
+++ Examen II_Andrea Lozano/Modelos/DAD/Conexion.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace Examen_II_Andrea_Lozano.Modelos.DAD
 {
-    class Conexion
+    public class Conexion
     {
+        protected SqlConnection MiConexion = new SqlConnection(ConfigurationManager.ConnectionStrings["ExamenIIConexion"].ConnectionString);
     }
 }
--- Examen II_Andrea Lozano/Modelos/DAD/UsuarioDAD.cs
+++ Examen II_Andrea Lozano/Modelos/DAD/UsuarioDAD.cs
@@ -1,12 +1,77 @@
-﻿using System;
+﻿using Examen_II_Andrea_Lozano.Modelos.Entidades;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Examen_II_Andrea_Lozano.Modelos.DAD
 {
-    class UsuarioDAD
+    public class UsuarioDAD : Conexion
     {
+        SqlCommand comando = new SqlCommand();
+
+        public bool ValidarUsuario(Usuario user)
+        {
+            bool valido = false;
+            try
+            {
+                StringBuilder sql = new StringBuilder();
+                sql.Append(" SELECT 1 FROM USUARIO WHERE EMAIL = @Email AND CLAVE = @Clave;");
+
+                comando.Connection = MiConexion;
+                MiConexion.Open();
+                comando.CommandType = System.Data.CommandType.Text;
+                comando.CommandText = sql.ToString();
+                comando.Parameters.Add("@Email", SqlDbType.NVarChar, 50).Value = user.Email;
+                comando.Parameters.Add("@Clave", SqlDbType.NVarChar, 150).Value = user.Clave;
+                valido = Convert.ToBoolean(comando.ExecuteScalar());
+                MiConexion.Close();
+            }
+            catch (Exception)
+            {
+            }
+            return valido;
+        }
+
+        public bool InsertarNuevoUsuario(Usuario user)
+        {
+            try
+            {
+                StringBuilder sql = new StringBuilder();
+                sql.Append(" INSERT INTO USUARIO ");
+                sql.Append(" VALUES (@Nombre, @Email, @Clave, @EsAdministrador); ");
+
+                comando.Connection = MiConexion;
+                MiConexion.Open();
+                comando.CommandType = System.Data.CommandType.Text;
+                comando.CommandText = sql.ToString();
+                comando.Parameters.Add("@Nombre", SqlDbType.NVarChar, 80).Value = user.Nombre;
+                comando.Parameters.Add("@Email", SqlDbType.NVarChar, 50).Value = user.Email;
+                comando.Parameters.Add("@Clave", SqlDbType.NVarChar, 80).Value = EncriptarClave(user.Clave);
+                comando.Parameters.Add("@EsAdministrador", SqlDbType.Bit).Value = user.EsAdministrador;
+                comando.ExecuteNonQuery();
+                return true;
+                MiConexion.Close();
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public static string EncriptarClave(string str)
+        {
+            string cadena = str + "MiClavePersonal";
+            SHA256 sha256 = SHA256Managed.Create();
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] stream = null;
+            StringBuilder sb = new StringBuilder();
+            stream = sha256.ComputeHash(encoding.GetBytes(cadena));
+            for (int i = 0; i < stream.Length; i++) sb.AppendFormat("{0:x2}", stream[i]);
+            return sb.ToString();
+        }
     }
 }
